d7c8e20e9d34b1043bc8a4f845c99df05f206d9a
Dionnell Riquelme <driquelme@globalterrenos.cl>
2026-08-27 11:45:51 -0400

    update primary container color in CSS and modify header link hover color for consistency

--- src/plantilla/components/Header.tsx
+++ src/plantilla/components/Header.tsx
@@ -45,7 +45,7 @@ export const Header = ({ data, onOpenMobileMenu }: HeaderProps) => {
           {data.links.map((link) => (
             <a
               key={link.id}
-              className="font-label-md text-label-xl font-bold text-[#0000ff] hover:text-[#0F0FFF] transition-colors"
+              className="font-label-md text-label-xl font-bold text-[#0000ff] hover:text-primary transition-colors"
               href={link.href}
             >
               {link.label}
